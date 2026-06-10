3469d0c9a2f49fe2f141e3e338ac3bc8151777b0
B-Kha <khabnhe182308@fpt.edu.vn>
2026-06-10 13:43:55 +0700

    create database

---
+++ src/main/java/com/example/bonsai_shop/BonsaiShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.bonsai_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonsaiShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BonsaiShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bonsai_shop/BonsaiShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bonsai_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonsaiShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
